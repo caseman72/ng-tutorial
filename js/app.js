4878a6ea3403d2d07a28a68861b1e3b4ceee8982
Casey Manion <casey@manion.com>
2014-02-03 00:33:52 -0800

    controller, app, changes

---
+++ js/app.js
@@ -0,0 +1,15 @@
+(function(ng) {
+	var ex4_app = ng.module("ex4-app", []);
+	ex4_app.controller("ex4-controller", ["$scope", "$http", "$timeout",
+		function($scope, $http, $timeout) {
+			$scope.seahawks = ["loading"];
+			$http.
+				get("/ajax/seahawks.json").
+				then(function(response) {
+					$timeout(function() {
+						$scope.seahawks = response.data || ["error"];
+					}, 1000, true);
+				});
+		}
+	]);
+})(window.angular);
